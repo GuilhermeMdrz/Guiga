2fbf1d22eb5a58925cf391777cd2b044b9bd2edd
Guilherme Medeiros <157317719+GuilhermeMdrz@users.noreply.github.com>
2024-09-05 15:36:42 -0300

    Update Aula02.py

---
+++ Introdução à Lógica e Programação/Aula02.py
@@ -0,0 +1,21 @@
+print('-=' * 10, 'Atividade 01', '-=' * 10,)
+num1 = 5.9
+num2 = 8.4
+média = (num1 + num2) / 2
+print(f'A média aritmética entre {num1} e {num2} é igual a {média}')
+print('-=' * 27)
+
+print('-=' * 10, 'Atividade 02', '-=' * 10,)
+salário = 800
+aumento = (salário * 15.8 / 100) + salário
+print(f'Após um aumento de 15.8% você receberá R${aumento}')
+print('-=' * 27)
+
+print('-=' * 10, 'Atividade 03', '-=' * 10,)
+A = 10
+B = 5
+C = A
+A = B
+B = C
+print(f'A é igual a {A}, e B é iual a {B}')
+print('-=' * 27)
